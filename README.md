a60ac313354c2c46ff55d89dabece536d3d9f09b
techconsulter <milacek@direction.at>
2020-08-11 08:06:09 +0200

    nitial push

---
+++ README.md
@@ -0,0 +1,17 @@
+# zsh
+ZSH Config adated 4 ubuntu with install script
+
+Based on the ZSH Config of ChrisTitus https://github.com/ChrisTitusTech/zsh
+
+## Setup
+```
+git clone --recursive https://github.com/techconsulter/zsh
+chmod +x ~/zsh/install.sh
+~/zsh/install.sh
+```
+## Dependencies 
+  - zsh-syntax-highlighting - installed by install script
+  - autojump - installed by install script
+  - zsh-autosuggestions - installed by install script
+  
+To use ZSH as your default shell: change your user in /etc/passwd from /bin/bash to /bin/zsh
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#link the zshrc file
+ln -s -f ~/zsh/.zshrc ~/.zshrc
+#install zsh and plugins needed
+sudo apt install zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+#powerlevel10k for zsh prompt
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+#Fonts download and install
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+#On Linux:
+mkdir -p /usr/share/fonts/truetype/Meslo
+cp *.ttf /usr/share/fonts/truetype/Meslo/
+fc-cache
