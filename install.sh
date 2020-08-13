2f6d960ac95139a94d2f1f5c7a900c81f5ed8a3d
techconsulter <milacek@direction.at>
2020-08-14 00:06:47 +0200

    changes for Ubuntu 18.04

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 ln -s -f ~/zsh/.zshrc ~/.zshrc
 #install zsh and plugins needed
 sudo apt install zsh zsh-syntax-highlighting
+#installing plugins via github
 cd /usr/share
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions
 sudo git clone git://github.com/wting/autojump.git
+
 #powerlevel10k for zsh prompt
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
@@ -21,3 +23,4 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20B
 sudo mkdir -p /usr/share/fonts/truetype/Meslo
 sudo cp *.ttf /usr/share/fonts/truetype/Meslo/
 fc-cache
+zsh
