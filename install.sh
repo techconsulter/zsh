a512e16322652aac3556cd94d9d83f7ca8eb51a3
techconsulter <milacek@direction.at>
2020-08-13 23:37:07 +0200

    changes for Ubuntu 18.04

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 #link the zshrc file
 ln -s -f ~/zsh/.zshrc ~/.zshrc
 #install zsh and plugins needed
-sudo apt install zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+sudo apt install zsh zsh-syntax-highlighting
+cd /usr/share
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions
+sudo git clone git://github.com/wting/autojump.git
 #powerlevel10k for zsh prompt
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
